e179d3265a82c5d808aeb1592273f882de2f63eb
eznedan <andriy.nedvyha@ericsson.com>
2018-05-31 19:14:27 +0300

    ADD: regExp

--- src/main/java/com/controller/impl/WebControllerImpl.java
+++ src/main/java/com/controller/impl/WebControllerImpl.java
@@ -1,13 +1,18 @@
 package com.controller.impl;
 
 import com.controller.WebController;
+import com.converter.ConvertToMongo;
 import com.model.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Component
 public class WebControllerImpl implements WebController {
+
+    @Autowired
+    private ConvertToMongo converter;
 
     @Override
     public String showIndex(Model model) {
@@ -17,7 +22,7 @@ public class WebControllerImpl implements WebController {
 
     @Override
     public String convertToMongoQuery(@ModelAttribute(value = "query") Query sqlQuery) {
-
+        converter.splitSqlQuery(sqlQuery.getSqlQuery().toLowerCase());
         return null;
     }
 }
--- src/main/java/com/converter/ConvertToMongo.java
+++ src/main/java/com/converter/ConvertToMongo.java
@@ -2,10 +2,21 @@ package com.converter;
 
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public class ConvertToMongo {
+    private static final String PATTERN_START = "(?<=";
+    private static final String PATTERN_BETWEEN = ")(.*\\n?)(?=";
+    private static final String PATTERN_FINISH = ")";
 
-    private String splitSqlQuery(String sqlQuery){
+    private String regExp(String startWord, String finishWord, String sqlQuery) {
+        Matcher matcher = Pattern.compile(PATTERN_START + startWord + PATTERN_BETWEEN + finishWord + PATTERN_FINISH).matcher(sqlQuery);
+        return matcher.find() ? matcher.toString() : null;
+    }
+
+    public String splitSqlQuery(String sqlQuery) {
         return sqlQuery;
     }
 }
